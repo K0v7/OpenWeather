34c977156c82465bb63f111f9bbbe41f5920ad4b
Felipe Silva <contato.felipesilva2020@Hotmail.com>
2022-10-27 18:00:04 -0300

    Update jJS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
-const apikey = "5c8467c230229e70055cce4b7bc8d9ea"
+// Variaveis e Seleção de elementos
+const apiKey = "5c8467c230229e70055cce4b7bc8d9ea";
+const apiCountryURL = "https://countryflagsapi.com/png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+//Funções
+
+const getWeatherData = async (city) => {
+    const apiWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics&appid=${apiKey}&lang=pt_br'
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json();
+
+    console.log(data);
+
+}
+
+const showWeatherData = (city) => {
+    getWeatherData(city);
+}
+
+//eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
