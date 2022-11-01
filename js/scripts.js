73f19e6fcd725104ff146e9f4127b21ee5fa2f5d
Felipe Silva <contato.felipesilva2020@hotmail.com>
2022-11-01 13:02:12 -0300

    Project Created

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Variaveis e Seleção de elementos
 const apiKey = "5c8467c230229e70055cce4b7bc8d9ea";
-const apiCountryURL = "https://countryflagsapi.com/png";
+const apiCountryURL = "https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -8,25 +8,35 @@ const searchBtn = document.querySelector("#search");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
-const weatherElement = document.querySelector("#weather-icon");
+const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 
 //Funções
 
 const getWeatherData = async (city) => {
-    const apiWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics&appid=${apiKey}&lang=pt_br'
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics&appid=${apiKey}&lang=pt_br`
 
     const res = await fetch(apiWeatherURL)
     const data = await res.json();
 
-    console.log(data);
+    return data
 
 }
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city)
+
+    cityElement.innerText = data.name
+    tempElement.innerText = parseInt(data.main.temp)
+    descElement.innerText = data.weather[0].description
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country)
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`
+
+
 }
 
 //eventos
