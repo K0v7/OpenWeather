addcf0ff00806373fe296e0196de6d3b53b2f87b
Felipe Silva <contato.felipesilva2020@Hotmail.com>
2022-11-04 16:39:29 -0300

    Ending Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // Variaveis e Seleção de elementos
 const apiKey = "5c8467c230229e70055cce4b7bc8d9ea"
 const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -10,40 +11,95 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const humidityElement = document.querySelector("#umidity span");
+const umidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 
-//Funções
+const weatherContainer = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
+
+// Loader
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+};
 
 const getWeatherData = async (city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics&appid=${apiKey}&lang=pt_br`
+    toggleLoader();
 
-    const res = await fetch(apiWeatherURL)
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
-    return data
+    toggleLoader();
 
-}
+    return data;
+};
+
+// Tratamento de erro
+const showErrorMessage = () => {
+    errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+    errorMessageContainer.classList.add("hide");
+    weatherContainer.classList.add("hide");
+
+    suggestionContainer.classList.add("hide");
+};
 
 const showWeatherData = async (city) => {
-    const data = await getWeatherData(city)
+    hideInformation();
 
-    cityElement.innerText = data.name
-    tempElement.innerText = parseInt(data.main.temp)
-    descElement.innerText = data.weather[0].description
-    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
-    countryElement.setAttribute("src", apiCountryURL + data.sys.country)
-    humidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`
+    const data = await getWeatherData(city);
 
+    if (data.cod === "404") {
+        showErrorMessage();
+        return;
+    }
 
-}
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    umidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
 
-//eventos
-searchBtn.addEventListener("click", (e) => {
+    // Change bg image
+    document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+    weatherContainer.classList.remove("hide");
+};
+
+searchBtn.addEventListener("click", async (e) => {
     e.preventDefault();
 
     const city = cityInput.value;
 
     showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+});
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const city = btn.getAttribute("id");
+
+        showWeatherData(city);
+    });
 });
